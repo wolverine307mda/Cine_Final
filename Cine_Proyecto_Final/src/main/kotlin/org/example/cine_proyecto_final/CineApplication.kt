c9fb7aa9fbb66c797f2f5083cb611e6185f1cc88
Diego NL <diegonoviluce28@gmail.com>
2024-05-20 15:30:10 +0200

    Arreglando lo CineApplicación

--- Cine_Proyecto_Final/src/main/kotlin/org/example/cine_proyecto_final/CineApplication.kt
+++ Cine_Proyecto_Final/src/main/kotlin/org/example/cine_proyecto_final/CineApplication.kt
@@ -7,7 +7,7 @@ import javafx.stage.Stage
 import org.example.cine_proyecto_final.config.AppConfig
 import org.example.cine_proyecto_final.controllers.GeneralBienvenidoController
 import org.example.cine_proyecto_final.database.SqlDeLightClient
-
+import org.example.cine_proyecto_final.database.logger
 
 class CineApplication : Application() {
 
@@ -15,35 +15,24 @@ class CineApplication : Application() {
     private lateinit var dbClient: SqlDeLightClient
 
     override fun start(stage: Stage) {
-        // Inicializar la configuración de la aplicación y el cliente de base de datos
-        initializeApp()
+        try {
+            // Initialize application configuration and database client
+            initializeApp()
 
-        // Cargar la escena de JavaFX
-        val fxmlLoader = FXMLLoader(CineApplication::class.java.getResource("/org/example/cine_proyecto_final/general_bienvenido_screen.fxml"))
-        val scene = Scene(fxmlLoader.load(), 320.0, 240.0)
-
-        // Obtener el controlador y pasar el cliente de base de datos
-        val controller = fxmlLoader.getController<GeneralBienvenidoController>()
-        controller.dbClient = dbClient
-
-        stage.title = "Hello!"
-        stage.scene = scene
-        stage.show()
-    }
 
 
     private fun initializeApp() {
-        // Inicializar configuración de la aplicación
+        // Initialize application configuration
         appConfig = AppConfig()
 
-        // Inicializar cliente de base de datos
+        // Initialize database client
         dbClient = SqlDeLightClient(appConfig)
 
-        // Puedes realizar otras inicializaciones aquí si es necesario
-        // Ejemplo: dbClient.dbQueries.someQuery()
+        // Additional initializations if necessary
     }
 }
 
 fun main() {
     Application.launch(CineApplication::class.java)
 }
+
