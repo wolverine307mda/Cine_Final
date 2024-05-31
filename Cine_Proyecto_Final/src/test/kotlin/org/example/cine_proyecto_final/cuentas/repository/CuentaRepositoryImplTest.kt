88b03d864d89f89fd93ee385a31e27374d0d93d1
Diego NL <diegonoviluce28@gmail.com>
2024-05-31 11:15:23 +0200

    Actualizo los tests

--- Cine_Proyecto_Final/src/test/kotlin/org/example/cine_proyecto_final/cuentas/repository/CuentaRepositoryImplTest.kt
+++ Cine_Proyecto_Final/src/test/kotlin/org/example/cine_proyecto_final/cuentas/repository/CuentaRepositoryImplTest.kt
@@ -49,6 +49,7 @@ class CuentaRepositoryImplTest {
                 apellido = "uno",
                 updatedAt = "2021-01-01T00:00:00.000",
                 createdAt = "2021-01-01T00:00:00.000",
+                imagen = "admin.jpg",
                 password = "contrasenia",
                 tipo = "ADMINISTRADOR"
             )
@@ -58,6 +59,7 @@ class CuentaRepositoryImplTest {
                 apellido = "uno",
                 updatedAt = "2021-01-01T00:00:00.000",
                 createdAt = "2021-01-01T00:00:00.000",
+                imagen = "useer.jpg",
                 password = "contrasenia2",
                 tipo = "USUARIO"
             )
@@ -83,6 +85,7 @@ class CuentaRepositoryImplTest {
             email = "admin@admin.com",
             nombre = "admin",
             apellido = "admin",
+            imagen = "admin.jpg",
             password = "password",
             tipo = TipoCuenta.ADMINISTRADOR,
             createdAt = LocalDateTime.parse("2023-01-01T00:00:00.000"),
@@ -104,6 +107,7 @@ class CuentaRepositoryImplTest {
             apellido = "uno",
             updatedAt = LocalDateTime.now(),
             createdAt = LocalDateTime.parse("2021-01-01T00:00:00.000"),
+            imagen = "admin.jpg",
             password = "contrasenia",
             tipo = TipoCuenta.ADMINISTRADOR
         )
@@ -119,23 +123,26 @@ class CuentaRepositoryImplTest {
             apellido = "apellido",
             updatedAt = LocalDateTime.now(),
             createdAt = LocalDateTime.parse("2021-01-01T00:00:00.000"),
+            imagen = "upAdmin.jpg",
             password = "Contrasenia",
             tipo = TipoCuenta.ADMINISTRADOR
         )
         val result = repository.update("admin@admin.com", cuenta)
         assertNotNull(result)
-        assertEquals("administrador", result.nombre)
-        assertEquals("apellido", result.apellido)
+        assertEquals("administrador", result?.nombre)
+        assertEquals("apellido", result?.apellido)
+        assertEquals("upAdmin.jpg", result?.imagen)
     }
 
     @Test
-    fun noActualizaUnaCuentaQueNoExiste() {
+    fun NoActualizaUnaCuentaQueNoExiste() {
         val cuenta = Cuenta(
             email = "noexiste@gmail.com",
             nombre = "no",
             apellido = "existe",
             updatedAt = LocalDateTime.now(),
             createdAt = LocalDateTime.now(),
+            imagen = "no.jpg",
             password = "password",
             tipo = TipoCuenta.USUARIO
         )
